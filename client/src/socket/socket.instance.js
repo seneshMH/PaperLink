5d3e5c2386f590cda09e43c0d03638b57dbd5acf
senesh <senesh1senesh@gmail.com>
2023-11-14 22:59:40 +0530

    Fixing socketIO

--- client/src/socket/socket.instance.js
+++ client/src/socket/socket.instance.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const ENDPOINT = `https://paperlink.onrender.com:${process.env.PORT}`;
+const ENDPOINT = "https://paperlink.onrender.com:10000";
 
 export const socket = io(ENDPOINT, {
     autoConnect: false,
