3b3d0304a08d0ef6f0b61adf897cc961392a1c19
SanjuMaduranga <sanjulamaduranga99@gmail.com>
2023-12-13 15:45:57 +0530

    creating admin dashboard

---
+++ client/src/pages/dashbord/admin/AdminDashbord.jsx
@@ -0,0 +1,160 @@
+import React from "react";
+import Products from "./products/Products";
+import Box from "@mui/material/Box";
+import Users from "./users/Users";
+import {
+  Container,
+  Divider,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import styled from "@emotion/styled";
+
+import MuiDrawer from "@mui/material/Drawer";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PeopleIcon from "@mui/icons-material/People";
+import EnergySavingsLeafIcon from "@mui/icons-material/EnergySavingsLeaf";
+import FeaturedVideoIcon from "@mui/icons-material/FeaturedVideo";
+import ReceiptIcon from "@mui/icons-material/Receipt";
+import AddCardIcon from "@mui/icons-material/AddCard";
+
+import Advertisments from "./advertisments/Advertisments";
+import PaperOrders from "./paperOrders/PaperOrders";
+import BidPayments from "./bidPayments/BidPayments";
+
+const drawerWidth = 240;
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    border: "none",
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
+
+function AdminDashboard() {
+  const pages = ["Products", "Users", "Advertisments", "PaperOrders", "BidPayments"];
+  const [page, setPage] = React.useState(pages[0]);
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        position: "fixed",
+      }}
+    >
+      <Drawer variant="permanent" open={open} sx={{ borderRight: "1px solid gray" }}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            px: [1],
+          }}
+        >
+          <EnergySavingsLeafIcon
+            sx={{
+              display: { xs: "none", md: "flex" },
+              mr: 1,
+            }}
+          />
+          <Typography variant="h5" noWrap component="div" p={2}>
+            Paper Link
+          </Typography>
+          <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+
+        <Divider />
+        <List component="nav" sx={{ height: "100%" }}>
+          <ListItemButton onClick={() => setPage("Products")}>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Products" />
+          </ListItemButton>
+          <Divider sx={{ my: 1 }} />
+          <ListItemButton onClick={() => setPage("Users")}>
+            <ListItemIcon>
+              <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Users" />
+          </ListItemButton>
+          <Divider sx={{ my: 1 }} />
+          <ListItemButton onClick={() => setPage("Advertisments")}>
+            <ListItemIcon>
+              <FeaturedVideoIcon />
+            </ListItemIcon>
+            <ListItemText primary="Advertisments" />
+          </ListItemButton>
+          <Divider sx={{ my: 1 }} />
+          <ListItemButton onClick={() => setPage("PaperOrders")}>
+            <ListItemIcon>
+              <ReceiptIcon />
+            </ListItemIcon>
+            <ListItemText primary="Paper Orders" />
+          </ListItemButton>
+          <Divider sx={{ my: 1 }} />
+          <ListItemButton onClick={() => setPage("BidPayments")}>
+            <ListItemIcon>
+              <AddCardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Bid Payments" />
+          </ListItemButton>
+          <Divider sx={{ my: 1 }} />
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[900],
+          flexGrow: 1,
+          width: `calc(100vw - ${open ? drawerWidth : 24}px)`,
+          height: "100vh",
+          overflow: "auto",
+        }}
+      >
+        <Container maxWidth="lg" sx={{ mt: 2, mb: 6 }}>
+          {page === "Products" && <Products />}
+          {page === "Users" && <Users />}
+          {page === "Advertisments" && <Advertisments />}
+          {page === "PaperOrders" && <PaperOrders />}
+          {page === "BidPayments" && <BidPayments />}
+        </Container>
+      </Box>
+    </Box>
+  );
+}
+
+export default AdminDashboard;
